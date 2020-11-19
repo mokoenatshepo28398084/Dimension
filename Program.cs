b2ec648bdcd24e2e094d578989277642171c3818
mokoenatshepo28398084 <47389808+mokoenatshepo28398084@users.noreply.github.com>
2020-11-19 13:56:08 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace DimensionData
+namespace Project
 {
     public class Program
     {
